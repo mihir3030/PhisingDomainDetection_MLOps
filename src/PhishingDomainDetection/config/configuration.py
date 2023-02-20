b937f21c4d58766da92aa2c52248eb223372fded
mihir3030 <mihirdholakia777@gmail.com>
2023-02-20 17:03:09 +0530

    trail updated

--- src/PhishingDomainDetection/config/configuration.py
+++ src/PhishingDomainDetection/config/configuration.py
@@ -11,6 +11,7 @@ class ConfigurationManager:
         self.params = read_yaml_file(params_file_path)
         create_directories([self.config.artifacts_root])
 
+
     def get_data_ingestion_config(self) -> DataIngestionConfig:
         config = self.config.data_ingestion
         create_directories([config.root_dir])
@@ -21,74 +22,51 @@ class ConfigurationManager:
         )
 
         return data_ingestion_config
-
-    def get_data_load_for_preprocess(self):
-        self.data_load_config = self.config.data_ingestion
-        self.root_dir = self.data_load_config.root_dir
-        self.file_dir = self.data_load_config.local_file
-        self.file_path = os.path.join(self.root_dir, self.file_dir)
-        return self.file_path
        
 
     def get_data_preprocess_config(self) -> DataPreprocessConfig:
-        config = self.config.data_preprocessing
-        create_directories([config.root_dir])
+        data_preprocessing = self.config.data_preprocessing
+        dataset_load = os.path.join(self.config.data_ingestion.root_dir, self.config.data_ingestion.local_file) 
+        create_directories([data_preprocessing.root_dir])
+
         data_preprocess_config = DataPreprocessConfig(
-            root_dir=Path(config.root_dir),
-            load_file_path=Path(self.get_data_load_for_preprocess()),
-            local_file=Path(config.local_file),
+            root_dir=Path(data_preprocessing.root_dir),
+            load_file_path=Path(dataset_load),
+            local_file=Path(data_preprocessing.local_file),
             params_select_columns=self.params.select_columns
         )
 
         return data_preprocess_config
-
-    def get_data_load_for_training(self):
-        self.data_load_config = self.config.data_preprocessing
-        self.root_dir = self.data_load_config.root_dir
-        self.file_dir = self.data_load_config.local_file
-        self.file_path = os.path.join(self.root_dir, self.file_dir)
-        return self.file_path
        
 
     def get_training_config(self) -> TrainingConfig:
-        config = self.config.training
-        
+        training = self.config.training
+        dataset_load = os.path.join(self.config.data_preprocessing.root_dir, self.config.data_preprocessing.local_file)
 
-        create_directories([config.root_dir])
+        create_directories([training.root_dir])
 
         training_config = TrainingConfig(
-            root_dir=Path(config.root_dir),
-            load_file_path=Path(self.get_data_load_for_training()),
-            test_data_save=Path(config.test_data_save),
-            model_save=Path(config.model_save)
+            root_dir=Path(training.root_dir),
+            load_file_path=Path(dataset_load),
+            test_data_save=Path(training.test_data_save),
+            model_save=Path(training.model_save)
         )
 
         return training_config
 
 
-    def get_model_for_evaluation(self):
-        self.data_load_config = self.config.training
-        self.root_dir = self.data_load_config.root_dir
-        self.file_dir = self.data_load_config.model_save
-        self.file_path = os.path.join(self.root_dir, self.file_dir)
-        return self.file_path
-
-    def get_test_data_for_evaluation(self):
-        self.data_load_config = self.config.training
-        self.root_dir = self.data_load_config.root_dir
-        self.file_dir = self.data_load_config.test_data_save
-        self.file_path = os.path.join(self.root_dir, self.file_dir)        
-        return self.file_path
-
     def get_evaluation_config(self) -> EvaluationConfig:
-        config = self.config.model_evaluation
-        create_directories([config.root_dir])
+        evaluation = self.config.model_evaluation
+        load_model = os.path.join(self.config.training.root_dir, self.config.training.model_save)
+        load_test_data = os.path.join(self.config.training.root_dir, self.config.training.test_data_save)
+
+        create_directories([evaluation.root_dir])
 
         evaluation_config = EvaluationConfig(
-            root_dir=Path(config.root_dir),
-            load_model_path=Path(self.get_model_for_evaluation()),
-            load_test_data=Path(self.get_test_data_for_evaluation()),
-            local_file=Path(config.local_file)
+            root_dir=Path(evaluation.root_dir),
+            load_model_path=Path(load_model),
+            load_test_data=Path(load_test_data),
+            local_file=Path(evaluation.local_file)
         )
         
         return evaluation_config
